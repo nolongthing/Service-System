f8aaf6d13f1a311e3bd0999a53859e11214116f0
nolongthing <18317773771@163.com>
2021-01-13 00:09:50 +0800

    添加全局样式+

--- Client/src/pages/index.tsx
+++ Client/src/pages/index.tsx
@@ -4,16 +4,17 @@ import styles from './index.less';
 import io from 'socket.io-client';
 
 export default () => {
-  useEffect(() => {
-    const socket = io();
-    socket.on('connect', () => {
-      console.log(socket.id);
-    });
-    socket.on('message', (data: string) => {
-      console.log(data);
-    })
-    socket.emit('message','message come from client')
-  }, [])
+  // useEffect(() => {
+  //   const socket = io();
+  //   socket.on('connect', () => {
+  //     console.log(socket.id);
+  //   });
+  //   socket.on('message', (data: string) => {
+  //     console.log(data);
+  //   })
+  //   socket.emit('message','message come from client')
+  // }, [])
+  
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
